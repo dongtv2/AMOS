f75f8d22558ce3a621e0617a82a8902e9e114bd8
dongthan <thanvandong@gmail.com>
2024-12-07 19:36:50 +0700

    Upload 3 files

---
+++ generated_functions.py
@@ -0,0 +1,5 @@
+# def get_wp_info(): # Prompt:  SELECT     wpno,     wpno_i  FROM wp_header  -- Count total WP   -- Count total WP closed   -- Count total WP in-process   WHERE     wp_header # UID: d36e432b-242f-45a2-b208-1e86811b787a
+def get_wp_info(*args, **kwargs):
+    from get_wp_info import get_wp_info
+    return get_wp_info(*args, **kwargs)
+
